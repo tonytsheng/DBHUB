89fd339ca4de5234d76eba69a10c0a2e10795ca5
Cloud User <ec2-user@ip-172-31-10-108.us-east-2.compute.internal>
2021-01-05 18:42:23 +0000

    customer orders

--- PG/postgresql_dms_user.sql
+++ PG/postgresql_dms_user.sql
@@ -1,5 +1,10 @@
 CREATE USER postgresql_dms_user WITH PASSWORD 'postgresql_dms_user'; 
 ALTER USER postgresql_dms_user WITH SUPERUSER;   
 grant rds_superuser to postgresql_dms_user
+GRANT CONNECT ON DATABASE fly1 TO postgresql_dms_user;
+GRANT USAGE ON SCHEMA fly1 TO postgresql_dms_user;
+GRANT SELECT ON ALL TABLES IN SCHEMA fly1 TO postgresql_dms_user;
+GRANT ALL ON SEQUENCES IN SCHEMA fly1 TO postgresql_dms_user;
+
              
 
