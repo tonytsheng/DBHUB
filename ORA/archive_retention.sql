77e2844c4b6020c3d0c008416b5c98102f40ab4b
Cloud User <ec2-user@ip-172-31-10-108.us-east-2.compute.internal>
2021-01-05 01:02:52 +0000

    PG

---
+++ ORA/archive_retention.sql
@@ -0,0 +1,1 @@
+exec rdsadmin.rdsadmin_util.set_configuration('archivelog retention hours',24);
--- PG/postgresql_dms_user.sql
+++ PG/postgresql_dms_user.sql
@@ -1,4 +1,5 @@
 CREATE USER postgresql_dms_user WITH PASSWORD 'postgresql_dms_user'; 
 ALTER USER postgresql_dms_user WITH SUPERUSER;   
+grant rds_superuser to postgresql_dms_user
              
 
