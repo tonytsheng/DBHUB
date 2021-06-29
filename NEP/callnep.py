4eed0b324781225eeac5b2efa14f5eaafd15d12a
EC2 Default User <ec2-user@ip-10-0-0-166.us-east-2.compute.internal>
2021-06-29 12:23:33 +0000

    updates

---
+++ NEP/callnep.py
@@ -0,0 +1,22 @@
+from __future__  import print_function  # Python 2/3 compatibility
+#import sys
+
+from gremlin_python import statics
+from gremlin_python.structure.graph import Graph
+from gremlin_python.process.graph_traversal import __
+from gremlin_python.process.strategies import *
+from gremlin_python.driver.driver_remote_connection import DriverRemoteConnection
+
+#user_query = sys.argv[1]
+#print ("query: " + user_query )
+
+graph = Graph()
+remoteConn = DriverRemoteConnection('wss://nep1002.cyt4dgtj55oy.us-east-2.neptune.amazonaws.com:8182/gremlin','g')
+g = graph.traversal().withRemote(remoteConn)
+
+result = g.V().has('code', 'BWI').valueMap(True).next()
+print ('+++')
+print ('+++ query:' ,  result)
+print ('+++')
+
+remoteConn.close()
