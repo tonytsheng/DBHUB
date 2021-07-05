862c6c543a8b0ee70834288dec3c16d3a7e57364
EC2 Default User <ec2-user@ip-10-0-0-166.us-east-2.compute.internal>
2021-07-05 15:25:38 +0000

    dms mon

---
+++ BASEDAT/SAMPLE_SCHEMAS/HUMAN_RESOURCES/ORA/ins.hr_countries.sql
@@ -0,0 +1,8 @@
+insert into human_resources.countries values ('GR','Greece',1);
+insert into human_resources.countries values ('CR','Croatia',1);
+insert into human_resources.countries values ('IC','Iceland',1);
+insert into human_resources.countries values ('AS','Austria',1);
+insert into human_resources.countries values ('HR','Hungary',1);
+insert into human_resources.countries values ('NO','Norway',1);
+commit;
+exit;
