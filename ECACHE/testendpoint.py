10fd440de173988e66a59706bd258d4ad7849e31
EC2 Default User <ec2-user@ip-10-0-0-166.us-east-2.compute.internal>
2021-07-20 23:45:10 +0000

    pg python

--- ECACHE/testendpoint.py
+++ ECACHE/testendpoint.py
@@ -1,4 +1,4 @@
 import redis
-client=redis.Redis.from_url('redis://ttsheng-ec1-001.26vdzn.0001.use2.cache.amazonaws.com:6379')
+client=redis.Redis.from_url('redis://ttsecnew-ro.26vdzn.ng.0001.use2.cache.amazonaws.com:6379')
 client.ping()
 quit()
