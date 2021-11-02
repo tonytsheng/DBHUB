e1b96bd5f05ea6bf02835a9f80b7a543370198bd
EC2 Default User <ec2-user@ip-10-0-0-166.us-east-2.compute.internal>
2021-11-02 14:18:09 +0000

    sybase

--- BASEDAT/SAMPLE_SCHEMAS/FLY1/SYBASE/ins.reservation.sql
+++ BASEDAT/SAMPLE_SCHEMAS/FLY1/SYBASE/ins.reservation.sql
@@ -1,22 +1,42 @@
-seatnumber=`echo $RANDOM %25 | head -c2`
-seatletter=`head /dev/urandom | tr -dc A-G | head -c1`
-flightno=`echo $RANDOM | head -c2`
-echo ${seatnumber}${seatletter}
-
 use fly1
 go
+
+declare @fname varchar(50)
+declare @lname varchar(50)
+declare @iata1 varchar(5)
+declare @iata2 varchar(5)
+declare @seatno int
+declare @seat varchar(10)
+declare @airline varchar(2)
+declare @flight varchar(5)
+declare @flightno int
+
+select top 1 last_name into #templname from employee order by newid()
+select @lname = (select last_name from #templname)
+select top 1 first_name into #tempfname from employee order by newid()
+select @fname = (select first_name from #tempfname)
+select top 1 iata_code into #tempiata1 from airport order by newid()
+select @iata1 = (select iata_code from #tempiata1)
+select top 1 iata_code into #tempiata2 from airport order by newid()
+select @iata2 = (select iata_code from #tempiata2)
+select @seatno = ( select (convert(int, round(rand() * 60,2))))
+select @seat = (select (cast(@seatno as varchar(10))) + 'F')
+select @airline = ( select char((rand()*25 + 65))+char((rand()*25 + 65)) )
+select @flightno = ( select (convert(int, round(rand() * 999,2))))
+select @flight = (select @airline + "123")
+
 insert into
 reservation
 (lname, fname, seatno, flightno, dep, arr, reservedate)
-values 
-( (select top 1 last_name from employee order by newid() ),
-  (select top 1 first_name from employee order by newid() ),
-  '${seatnumber}${seatletter}',
-  (select top 1 concat (abbreviation, '${flightno}') from airline order by newid() ),
-  (select top 1 iata_code from airport order by newid() ),
-  (select top 1 iata_code from airport order by newid() ),
-  getdate())
+values
+( @lname,
+  @fname,
+  @seat , 
+  @flight,
+  @iata1,
+  @iata2,
+  getdate()
+)
 go
 select count(*) from reservation
 go
-EQ
