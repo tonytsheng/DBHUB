cd61dfc9dd95f40cc8c54adcb53d470820d0fc47
EC2 Default User <tonytsheng@gmail.com>
2022-04-07 20:49:57 +0000

    esri

---
+++ 20220405.ESRI/fixsource.sql
@@ -0,0 +1,5 @@
+JEFFDM/ttsora10> delete from esrifeatureclass where objectid=34;
+JEFFDM/ttsora10> delete from esrifeatureclass where objectid=54;
+JEFFDM/ttsora10> delete from esrifeatureclass where objectid=53;
+JEFFDM/ttsora10> delete from esrifeatureclass where objectid=52;
+
---
+++ 20220405.ESRI/refresh.sql
@@ -0,0 +1,8 @@
+drop index "JEFFDM"."SHAPE_142114_2_SIDX" ;
+delete from esrifeatureclass;
+commit;
+insert into esrifeatureclass select * from esritestcopy;
+commit;
+CREATE INDEX "JEFFDM"."SHAPE_142114_2_SIDX" ON "JEFFDM"."ESRIFEATURECLASS" ("SHAPE")
+INDEXTYPE IS "MDSYS"."SPATIAL_INDEX"  PARAMETERS ('SDO_COMMIT_INTERVAL = 1000 ');  
+
