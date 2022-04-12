a02c236a41ed3a95eee55f4b316735a29c43da2c
EC2 Default User <tonytsheng@gmail.com>
2022-04-12 15:21:12 +0000

    esri

---
+++ SCRIPTLIB/aswin.sql
@@ -0,0 +1,27 @@
+set serveroutput on;
+DECLARE
+  v_hdnl NUMBER;
+BEGIN
+  v_hdnl := DBMS_DATAPUMP.OPEN( operation => 'EXPORT', job_mode => 'SCHEMA', job_name=>null);
+  dbms_output.put_line (v_hdnl);
+  DBMS_DATAPUMP.ADD_FILE(
+    handle    => v_hdnl,
+    filename  => 'esri.dmp',
+    directory => 'DATA_PUMP_DIR',
+    filetype  => dbms_datapump.ku$_file_type_dump_file,
+    reusefile => 1);
+  DBMS_DATAPUMP.ADD_FILE(
+    handle    => v_hdnl,
+    filename  => 'esri.log',
+    directory => 'DATA_PUMP_DIR',
+    filetype  => dbms_datapump.ku$_file_type_log_file,
+    reusefile => 1);
+  dbms_output.put_line ('Post add file');
+  DBMS_DATAPUMP.METADATA_FILTER(v_hdnl,'SCHEMA_EXPR','IN (''JEFFDM'')');
+  dbms_output.put_line ('Post metadata filter ');
+  DBMS_DATAPUMP.START_JOB(v_hdnl);
+END;
+/
+SELECT * FROM TABLE(rdsadmin.rds_file_util.listdir(p_directory => 'DATA_PUMP_DIR'));
+exit;
+
