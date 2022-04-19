4f55d759e5f0d6cfd08fe40df1803da82c2070ab
EC2 Default User <tonytsheng@gmail.com>
2022-04-19 13:45:16 +0000

    python

---
+++ PYTHON.BABY/writealog.py
@@ -0,0 +1,8 @@
+import logging
+import time
+
+# Set up logging
+logging.basicConfig(level=logging.INFO,format='%(asctime)s: %(message)s', datefmt='%m/%d/%Y %H:%M:%S ')
+sheng='sheng'
+logging.info('tony sheng')
+
