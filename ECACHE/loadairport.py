1f18c179f5e7587c818e59a6dddfbead725d207a
EC2 Default User <tonytsheng@gmail.com>
2022-04-29 08:47:15 +0000

    ecache

---
+++ ECACHE/loadairport.py
@@ -0,0 +1,48 @@
+import os
+import pymysql
+import psycopg2
+import boto3
+import base64
+import json
+from botocore.exceptions import ClientError
+import redis
+import logging
+import time
+import glob
+import csv
+
+def loadcache (filename, secs):
+    logging.info("begin script")
+    files = glob.glob(filename)
+    for filename in files:
+        with open(filename, 'r', encoding="utf-8") as csvfile:
+            myreader = csv.reader(csvfile, delimiter=',')
+            for row in myreader:
+                newitem = {}
+                keyName=row[1]
+                keyValues={'name':row[0]}
+                print("%s : %s %s"  % (filename, keyName, keyValues))
+                cache.hset(keyName, mapping=keyValues)
+                cache.expire(keyName, 604800)
+    logging.info("cache load done")
+
+
+def checkcache ():
+    #keyValues=cache.hgetall()
+    keyValues=cache.keys("*")
+    logging.info("checking cache ")
+    for k in enumerate(keyValues):
+        logging.info("cache check {} ".format(k))
+
+## main
+logging.basicConfig(level=logging.INFO,format='%(asctime)s: %(message)s', datefmt='%m/%d/%Y %H:%M:%S ')
+
+# Initialize the cache
+ttl = 10
+redis_url = 'redis://ttsecnew.26vdzn.ng.0001.use2.cache.amazonaws.com:6379'
+cache = redis.Redis.from_url(redis_url)
+
+loadcache("/home/ec2-user/DBHUB/BASEDAT/SAMPLE_SCHEMAS/FLY1/air2.csv", 604800)
+
+checkcache()
+
