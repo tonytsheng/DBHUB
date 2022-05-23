0218239fdff534d29774c6bc6c1070a46ad6e549
EC2 Default User <ec2-user@ip-10-0-2-111.us-east-2.compute.internal>
2022-05-23 14:48:37 +0000

    base dat ins products

--- BASEDAT/SAMPLE_SCHEMAS/CUSTOMER_ORDERS/PG/ins.products.sql
+++ BASEDAT/SAMPLE_SCHEMAS/CUSTOMER_ORDERS/PG/ins.products.sql
@@ -1,6 +1,7 @@
-insert into customer_orders.products
-values
-(200
-  ,'TTS'
-  ,34.56
-  ,'PRODUCTONE');
+insert into customer_orders.products (PRODUCT_ID,PRODUCT_NAME,UNIT_PRICE,PRODUCT_DETAILS)
+values ( (select (max(product_id))+1 from customer_orders.products)
+        ,'PRODUCT 1'
+        , NULL
+        , NULL
+);
+select count(*) from customer_orders.products;
