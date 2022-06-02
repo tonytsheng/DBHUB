895992ec4207d2ee78b5e6aa3aa56aafdf84013c
EC2 Default User <ec2-user@ip-10-0-2-111.us-east-2.compute.internal>
2022-06-02 17:56:27 +0000

    maz

--- 20220601.MAZ/connect.ora.py
+++ 20220601.MAZ/connect.ora.py
@@ -5,10 +5,10 @@
 import cx_Oracle
 import os
 import socket
-
+import datetime
 #from config import config
 
-def connect():
+def execute():
     """ Connect to the Oracle database server """
     conn = None
     try:
@@ -17,29 +17,35 @@ def connect():
                 dsn="ttsm100.ciushqttrpqx.us-east-2.rds.amazonaws.com:1521/ttsm100")
 		
         cur = conn.cursor()
-        
-        print('count from customer_orders.products')
-        sql = "insert into heartbeat values (%s, %s, %s )"
-        val = ("seq_heartbeat.nextval", "sysdate", (ip))
-
-        print (sql)
-
-
-#        cur.execute('SELECT count(*) from customer_orders.products')
-        cur.execute(sql, val)
-        query_out = cur.fetchone()
-        print(query_out)
-        print(ip)
-
+        sql = "insert into heartbeat values (seq_heartbeat.nextval, :updatedate, :ip)"
+        cur.execute(sql, updatedate=datetime.datetime.now(), ip=(ip))
         cur.close()
-#    except (Exception, psycopg2.DatabaseError) as error:
-#        print(error)
+        conn.commit()
+        print ("insert ::: ", ip)
+
+    except (Exception, cx_Oracle.DatabaseError) as error:
+        print("Database Error ::: ", error)
     finally:
         if conn is not None:
             conn.close()
 
+def connect():
+    dsn = "(DESCRIPTION=(CONNECT_TIMEOUT=10)(ADDRESS=(PROTOCOL=TCP)(HOST=ttsm100.ciushqttrpqx.us-east-2.rds.amazonaws.com)(PORT=1521))(CONNECT_DATA=(SID=ttsm100)))"
+    try:
+        conn = cx_Oracle.connect(user="customer_orders",
+            password="Pass1234",
+            dsn=dsn)
+#            dsn="ttsm100.ciushqttrpqx.us-east-2.rds.amazonaws.com:1521/ttsm100")
+    except cx_Oracle.DatabaseError as e:
+            # Log error as appropriate
+        raise
+
+        # If the database connection succeeded create the cursor
+        # we-re going to use.
+
 
 if __name__ == '__main__':
     ip = socket.gethostbyname('ttsm100.ciushqttrpqx.us-east-2.rds.amazonaws.com')
+    print (datetime.datetime.now())
     connect()
 
