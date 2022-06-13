62179d394a7cb756207768dfe36e36cc26661e31
EC2 Default User <ec2-user@ip-10-0-2-111.us-east-2.compute.internal>
2022-06-13 13:04:22 +0000

    dms 2way

--- DMS/2WAYPOC/cr8.ora.heart.sql
+++ DMS/2WAYPOC/cr8.ora.heart.sql
@@ -1,10 +1,15 @@
 drop table heartbeat;
+drop sequence seq_heartbeat;
+
 create table heartbeat (
   heartbeat_id          integer,
   description             varchar(40) ,
   last_update_dt            timestamp ,
   last_update_site     varchar(5));
-CREATE SEQUENCE seq_heartbeat INCREMENT BY 1 START WITH 100;
+
+-- Remember, sequence needs to be seeded across sites.
+CREATE SEQUENCE seq_heartbeat INCREMENT BY 1 START WITH 10000;
+
 create unique index idxu_heartbeat on heartbeat(heartbeat_id);
 exit
 
--- DMS/2WAYPOC/cr8.pg.heart.sql
+++ DMS/2WAYPOC/cr8.pg.heart.sql
@@ -1,10 +1,14 @@
 drop table customer_orders.heartbeat;
+drop sequence customer_orders.seq_heartbeat;
+
 create table customer_orders.heartbeat (
   heartbeat_id          integer,
   description             varchar(40) ,
   last_update_dt            timestamp ,
   last_update_site     varchar(5));
-CREATE SEQUENCE customer_orders.seq_heartbeat INCREMENT 1 START 100;
+
+-- Remember, sequence needs to be seeded across sites.
+CREATE SEQUENCE customer_orders.seq_heartbeat INCREMENT 1 START 20000;
 create unique index idxu_heartbeat on customer_orders.heartbeat(heartbeat_id);
 \q
 
