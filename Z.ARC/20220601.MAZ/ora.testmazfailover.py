ca712a33f22f3995749ddb9e10421f8f0e324d73
EC2 Default User <ec2-user@ip-10-0-2-111.us-east-2.compute.internal>
2022-06-27 02:33:42 +0000

    maz test

--- Z.ARC/20220601.MAZ/ora.testmazfailover.py
+++ Z.ARC/20220601.MAZ/ora.testmazfailover.py
@@ -38,17 +38,16 @@ def connect():
         cur = conn.cursor()
         cur.execute(sql)
         records = cur.fetchone()
-        for row in records:
-            print (row)
         cur.close()
     except cx_Oracle.DatabaseError as e:
         print("Database Error ::: ", e)
+    return records
 
 
 if __name__ == '__main__':
     dsn =  "(DESCRIPTION=(CONNECT_TIMEOUT=10)(ADDRESS=(PROTOCOL=TCP)(HOST=ttsora30.ciushqttrpqx.us-east-2.rds.amazonaws.com)(PORT=1521))(CONNECT_DATA=(SID=ttsora30)))"
-    print (datetime.datetime.now())
+    timestamp = str(datetime.datetime.now())
     ip = socket.gethostbyname('ttsora30.ciushqttrpqx.us-east-2.rds.amazonaws.com')
-    print (ip)
-    connect()
+    dbcheck = str(connect())
+    print (timestamp + " ::: " + ip + " ::: " + dbcheck)
 
