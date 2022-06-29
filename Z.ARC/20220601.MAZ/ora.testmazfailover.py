b17df5d3dc07441e5c0dcabfdde5c01b16d1ce5d
EC2 Default User <ec2-user@ip-10-0-2-111.us-east-2.compute.internal>
2022-06-29 13:06:35 +0000

    maz test

--- Z.ARC/20220601.MAZ/ora.testmazfailover.py
+++ Z.ARC/20220601.MAZ/ora.testmazfailover.py
@@ -1,5 +1,33 @@
 #!/usr/bin/env python3
-# to run: python3 connect.ora.py
+#----------------------------------------------------------------------------
+# Created By  : Tony Sheng ttsheng@amazon.com  
+# Created Date: 2022-06-29
+# version ='1.0'
+# ---------------------------------------------------------------------------
+#
+# ora.testmazfailover.py
+# Test Multi AZ Failover by
+#   querying for database name
+#   getting IP for database endpoint
+#   getting status of database
+#   output of all of this one line
+#
+# ---------------------------------------------------------------------------
+#
+# To run:
+# Edit your database endpoints
+#    $ python3 ora.testmazfailover.py
+# Alternatively, run this within a while loop:
+#    $ while true;do;python3 ora.testmazfailover.py;sleep 1;done
+# It also may help to pipe the output to a logfile
+#
+# ---------------------------------------------------------------------------
+# 
+# History
+# 2022-06-29 - Version 1.0
+# ---------------------------------------------------------------------------
+#
+# ToDo
 
 #import psycopg2
 import cx_Oracle
@@ -39,18 +67,23 @@ def connect():
         cur = conn.cursor()
         cur.execute(sql)
         records = cur.fetchone()
+        if records: 
+            return records
+        else:                           # this block returns right away on database conn error
+            print("Database Error ")
+            return False
         cur.close()
     except cx_Oracle.DatabaseError as e:
         print("Database Error ::: ", e)
-    return records
+        return False
 
 
 if __name__ == '__main__':
     dsn =  "(DESCRIPTION=(CONNECT_TIMEOUT=10)(ADDRESS=(PROTOCOL=TCP)(HOST=ttsora30.ciushqttrpqx.us-east-2.rds.amazonaws.com)(PORT=1521))(CONNECT_DATA=(SID=ttsora30)))"
     timestamp = str(datetime.datetime.now())
     ip = socket.gethostbyname('ttsora30.ciushqttrpqx.us-east-2.rds.amazonaws.com')
-    dbstatus1 = subprocess.run(['/usr/bin/aws rds describe-db-instances --profile dba --db-instance-identifier ttsora30 | grep DBInstanceStatus'], shell=True, text=True, stdout=PIPE)
-    dbstatus = dbstatus1.communicate()
+    instance_stat = subprocess.run(['/usr/bin/aws rds describe-db-instances --profile dba --db-instance-identifier ttsora30 | jq -jr \' .DBInstances[] | .DBInstanceStatus \' ' ], shell=True, text=True, stdout=PIPE)
+    instance_status = instance_stat.stdout
     dbcheck = str(connect())
-    print (timestamp + " ::: " + ip + " ::: " + " ::: " + dbstatus + " ::: " + dbcheck)
+    print (timestamp + " ::: " + ip + " ::: " + instance_status + " ::: " + dbcheck)
 
