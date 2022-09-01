da9771c572aa24b14fd88df70af1e29ab6fd01e7
EC2 Default User <tonytsheng@gmail.com>
2022-09-01 23:44:08 +0000

    autotrace

---
+++ PERFTUNE/ora.autotrace.sql
@@ -0,0 +1,5 @@
+SET AUTOTRACE OFF No AUTOTRACE report is generated. This is the default.
+SET AUTOTRACE ON EXPLAIN The AUTOTRACE report shows only the optimizer execution path.
+SET AUTOTRACE ON STATISTICS The AUTOTRACE report shows only the SQL statement execution statistics.
+SET AUTOTRACE ON The AUTOTRACE report includes both the optimizer execution path and the SQL statement execution statistics.
+SET AUTOTRACE TRACEONLY Like SET AUTOTRACE ON, but suppresses the printing of the user's query output, if any.
