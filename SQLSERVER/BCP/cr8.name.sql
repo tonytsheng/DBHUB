4bbfc169da91fd87e5fd66dbb4e9db39d4f82327
EC2 Default User <tonytsheng@gmail.com>
2022-09-05 22:41:09 +0000

    sqlserver bcp

---
+++ SQLSERVER/BCP/cr8.name.sql
@@ -0,0 +1,6 @@
+nconst (string) - alphanumeric unique identifier of the name/person
+primaryName (string)– name by which the person is most often credited
+birthYear – in YYYY format
+deathYear – in YYYY format if applicable, else '\N'
+primaryProfession (array of strings)– the top-3 professions of the person
+knownForTitles (array of tconsts) – titles the person is known for
---
+++ SQLSERVER/BCP/readme.md
@@ -0,0 +1,2 @@
+## Quick BCP Example
+- Scripts and files to support a quick example for using Bulk Copy Program (BCP) with RDS for MS SQL Server.
