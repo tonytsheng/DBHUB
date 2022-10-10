3133854bd48b7544bf5f2714ea53376b6c94d8df
EC2 Default User <tonytsheng@gmail.com>
2022-10-10 23:24:59 +0000

    slob

--- SCRIPTLIB/ora.alterdatafile.sql
+++
@@ -1,1 +0,0 @@
-alter database datafile '/u01/app/oracle/oradata/ORADEV/datafile/slob_data_01.dbf' resize 5000M
--- SCRIPTLIB/ora.datafiles.sql
+++ SCRIPTLIB/ora.datafiles.sql
@@ -1,1 +1,4 @@
 select TABLESPACE_NAME, FILE_NAME,AUTOEXTENSIBLE,MAXBYTES from dba_Data_files where TABLESPACE_NAME='TTSHENG';
+
+alter database datafile '/u01/app/oracle/oradata/ORADEV/datafile/slob_data_01.dbf' resize 5000M
+
