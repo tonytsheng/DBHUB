fc8904147b7f866b91141d47dbfd9325f23d8cab
EC2 Default User <tonytsheng@gmail.com>
2022-10-27 19:58:28 +0000

    dms agent

--- SCRIPTLIB/ora.datafiles.sql
+++ SCRIPTLIB/ora.datafiles.sql
@@ -2,3 +2,9 @@ select TABLESPACE_NAME, FILE_NAME,AUTOEXTENSIBLE,MAXBYTES from dba_Data_files wh
 
 alter database datafile '/u01/app/oracle/oradata/ORADEV/datafile/slob_data_01.dbf' resize 5000M
 
+shutdown abort
+startup mount
+alter database datafile '/directory/filename' offline drop;
+alter database open;
+drop tablespace ts_name;
+
