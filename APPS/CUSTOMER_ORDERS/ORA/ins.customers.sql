9d667134242bdd0968cab5d5ad7ed43ea12b1ad7
EC2 Default User <tonytsheng@gmail.com>
2023-03-27 19:18:37 +0000

    web

---
+++ APPS/CUSTOMER_ORDERS/ORA/ins.customers.sql
@@ -0,0 +1,9 @@
+insert into  customers (
+  email_address   ,
+  full_name       )
+values
+('rd@gmail.com',
+	'rd');
+exit
+
+
---
+++ WEB/deploy.cmd
@@ -0,0 +1,9 @@
+# npm is the most widely used package manager for javascript projects
+# and includes node.js by default
+# node.js is a tool for building using javascript
+# nginx is a web server
+
+sudo service nginx restart | status
+npm install
+npm run build
+
