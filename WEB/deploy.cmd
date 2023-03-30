73b6ff3c7929709ac2e38be662d8db9fd2cb0af8
EC2 Default User <tonytsheng@gmail.com>
2023-03-30 05:25:32 +0000

    web

--- WEB/deploy.cmd
+++ WEB/deploy.cmd
@@ -3,7 +3,15 @@
 # node.js is a tool for building using javascript
 # nginx is a web server
 
+# web
 sudo service nginx restart | status
 npm install
+cd ~/web-tier
 npm run build
+
+# app 
+cd ~/app-tier
+npm install
+pm2 stop
+pm2 start
 
