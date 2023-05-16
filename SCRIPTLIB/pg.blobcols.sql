8f987983b577e3f47c06e61938d188168627bbec
EC2 Default User <tonytsheng@gmail.com>
2023-05-16 13:40:56 +0000

    pg blob cols

---
+++ SCRIPTLIB/pg.blobcols.sql
@@ -0,0 +1,13 @@
+select tab.table_name,
+    count(*) as columns
+from information_schema.tables as tab
+    inner join information_schema.columns as col
+        on col.table_schema = tab.table_schema
+        and col.table_name = tab.table_name
+        and col.data_type in ('blob', 'mediumblob', 'longblob', 
+                               'text', 'mediumtext', 'longtext')
+where tab.table_schema = 'your database name'
+      and tab.table_type = 'BASE TABLE'
+group by tab.table_name
+order by tab.table_name;
+
