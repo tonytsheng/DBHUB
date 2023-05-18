c2e50a77fe7bc1f246aea881f039336c2553c89e
EC2 Default User <tonytsheng@gmail.com>
2023-05-18 15:41:17 +0000

    automate dms from rr

---
+++ DMS/AUTOMATE_ON_FROM_RR/readme.md
@@ -0,0 +1,7 @@
+# Automating Turning on a DMS Migration Task After Promoting a Read Replica
+
+
+decribe db instances
+'ReadReplicaDBInstanceIdentifiers': [],
+'ReadReplicaSourceDBInstanceIdentifier': 'ttsora10',
+
---
+++ DMS/AUTOMATE_ON_FROM_RR/turnon.py
@@ -0,0 +1,173 @@
+#!/usr/bin/python3
+
+#----------------------------------------------------------------------------
+# Created By  : Tony Sheng ttsheng@amazon.com
+# Created Date: 2023-05-18
+# version ='1.0'
+# ---------------------------------------------------------------------------
+#
+# turnon.py
+# Promote a Read Replica and Turn on a DMS migration task
+#   Prereqs:
+#     A running replication instancea
+#     A source database and it's appropriate Read Replica
+#
+# ---------------------------------------------------------------------------
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# Ensure you apply your due diligence and test to your satisfaction
+# before running this code in a Production system.
+# ---------------------------------------------------------------------------
+#
+# To run:
+#    $ python3 rds.ora.expdp.py SCHEMA
+#
+# ---------------------------------------------------------------------------
+#
+# History
+# 2023-05-18 - Version 1.0
+# ---------------------------------------------------------------------------
+#
+# ToDo
+#   some sort of error checking
+
+
+import boto3
+import base64
+import json
+import decimal
+import sys
+import getopt
+import cx_Oracle
+from botocore.exceptions import ClientError
+from boto3.dynamodb.conditions import Key
+from datetime import date
+from datetime import datetime
+import time
+import datetime
+
+def wait():
+    for i in range(12):
+        now = datetime.datetime.now()
+        date_time = now.strftime("%d.%m.%Y %H:%M:%S")
+        print(date_time)
+        time.sleep (5)
+
+def get_secret():
+#    secret_name = "arn:aws:secretsmanager:us-east-2:070201068661:secret:secret-pg102-WNHBUK"
+    secret_name = "arn:aws:secretsmanager:us-east-2:070201068661:secret:pg102-secret-IZWCR2"
+    region_name = "us-east-2"
+    session = boto3.session.Session()
+    session = boto3.session.Session(profile_name='ec2')
+    client = session.client(
+      service_name='secretsmanager'
+        )
+    get_secret_value_response = client.get_secret_value(
+                SecretId=secret_name
+            )
+
+    database_secrets = json.loads(get_secret_value_response['SecretString'])
+    password = database_secrets['password']
+    return (password)
+
+def promote_rr():
+    response = client.promote_read_replica(
+        BackupRetentionPeriod=10,
+        DBInstanceIdentifier='mydbreadreplica',
+        PreferredBackupWindow='03:30-04:00',
+)
+
+#SCHEMA=(sys.argv[1])
+now = datetime.datetime.now()
+TIMESTAMP = now.strftime("%d.%m.%Y %H:%M:%S")
+#LOGFILE = SCHEMA+ ".exp.log"
+#DUMPFILE = SCHEMA + ".dmp"
+#print ("+++ Expdp logfile: " + LOGFILE)
+
+conn = None
+
+sql_rr_latency = """ select sequence#
+,thread#
+,status
+, to_char(first_time, 'MM/DD/YYYY HH24:MI:SS') FIRST
+, to_char(next_time, 'MM/DD/YYYY HH24:MI:SS') NEXT
+, applied
+, archived
+from v$archived_log
+where first_time > sysdate-1
+and dest_id=2
+order by sequence#
+"""
+## dest_id may need to be modified in the query above
+
+sql_get_scn = """ Select CURRENT_SCN from v$database """
+
+db_pw = get_secret()
+conn = cx_Oracle.connect(user='admin'
+         , password=db_pw
+         , dsn='ttsora10.ciushqttrpqx.us-east-2.rds.amazonaws.com:1521/ttsora10')
+
+cur = conn.cursor()
+#cur.execute(sql_exp)
+#time.sleep (10)
+
+cur.execute(sql_rr_latency)
+records = cur.fetchall()
+for row in records:
+    print ("+++ Archived Log Status: " + str(row))
+# row_count = cur.rowcount
+
+cur.execute(sql_get_scn)
+records = cur.fetchall()
+for row in records:
+    print ("+++ SCN : " + str(row))
+cur.close()
+
+session = boto3.session.Session()
+session = boto3.session.Session(profile_name='dba')
+client = session.client(
+      service_name='rds'
+        )
+#response = client.promote_read_replica(
+#        BackupRetentionPeriod=5,
+#        DBInstanceIdentifier='ttsora10-rr',
+#        )
+#print(response)
+
+pridb = "ttsora10"
+rrdb = "ttsora10b"
+
+dbs = ["ttsora10", "ttsora10-rr", "ttsora10b"]
+for db in dbs:
+#    print(db)
+    dbstatus = client.describe_db_instances(
+        DBInstanceIdentifier=db
+        )
+    print(dbstatus)
+    print(db + " : " + dbstatus['DBInstances'][0]['DBInstanceStatus'])
+    print(db + " : " + dbstatus['DBInstances'][0]['DBInstanceStatus'])
+
+
+
+
+#print(row_count)
+#while row_count >=1 :
+#    time.sleep (5)
+#    cur.execute(sql_chk_status)
+#    records = cur.fetchall()
+#    for row in records:
+#        print ("+++ Waiting for job: " + str(row))
+#    row_count=cur.rowcount
+#    print ( row_count)
+
+#cur.execute(sql_cat_log)
+#records = cur.fetchall()
+#for row in records:
+#    print (row)
+
+
+
+
