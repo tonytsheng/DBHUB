2d73bd6329de4adf0b4d219f2747ee7014f7e8c3
EC2 Default User <tonytsheng@gmail.com>
2023-05-29 21:42:13 +0000

    best python examples

---
+++ APPS/CUSTOMER_ORDERS/ORA/1.sql
@@ -0,0 +1,14 @@
+set linesize 200;
+set pagesize 25;
+col product_id format 999999;
+col product_name format a20;
+col image_last_updated format a30;
+alter session set nls_date_format = 'MM/DD/YYYY HH24:MI:SS';
+select * from 
+(select PRODUCT_ID,PRODUCT_NAME, IMAGE_LAST_UPDATED from admin.products where image_last_updated is not null 
+	order by IMAGE_LAST_UPDATED desc) h1  
+where rownum <=10 order by rownum;  
+
+select count(*) from admin.products;
+exit
+
