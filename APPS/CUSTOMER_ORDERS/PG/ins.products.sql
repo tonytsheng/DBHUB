73ec83e5841afd709640ed8e92eeafe7a65a9f3d
EC2 Default User <tonytsheng@gmail.com>
2023-06-14 18:49:51 +0000

    promote and create dms target

--- APPS/CUSTOMER_ORDERS/PG/ins.products.sql
+++ APPS/CUSTOMER_ORDERS/PG/ins.products.sql
@@ -1,8 +1,9 @@
-insert into customer_orders.products (PRODUCT_ID,PRODUCT_NAME,UNIT_PRICE,PRODUCT_DETAILS)
+insert into customer_orders.products (PRODUCT_ID,PRODUCT_NAME,UNIT_PRICE,PRODUCT_DETAILS, image_last_updated)
 values ( (select (max(product_id))+1 from customer_orders.products)
         ,(  select product_name from customer_orders.products order by random() limit 1)
 --	'PRODUCT 1'
         , NULL
         , NULL
+	, now()
 );
 select count(*) from customer_orders.products;
