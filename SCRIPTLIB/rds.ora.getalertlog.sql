efb8a3fdd10e94869c9971b78f47d664b922d4fe
EC2 Default User <tonytsheng@gmail.com>
2023-08-23 16:54:49 +0000

    ora_ssl

--- SCRIPTLIB/rds.ora.getalertlog.sql
+++ SCRIPTLIB/rds.ora.getalertlog.sql
@@ -2,6 +2,6 @@ col ts format a20
 set linesize 200
 set pagesize 0 
 select to_char(ORIGINATING_TIMESTAMP, 'MM/DD/YYY HH24:MI:SS') TS, message_text from alertlog
-where ORIGINATING_TIMESTAMP > sysdate-(24/24);
+where ORIGINATING_TIMESTAMP > sysdate-(1/24);
 exit
 
