6b745c1888fa5d7a38611a9c6733dde68f1819fd
EC2 Default User <tonytsheng@gmail.com>
2023-09-26 14:22:07 +0000

    dms promote step func

--- DMS/PROMOTE_CREATE_DMS_TGT/STEPFUNC/RDS_desc_db_instances.py
+++ DMS/PROMOTE_CREATE_DMS_TGT/STEPFUNC/RDS_desc_db_instances.py
@@ -4,10 +4,17 @@ import os
 client = boto3.client('rds')
 
 def lambda_handler(event, context):
+    
+    dbname = event["DbInstance"]["DbInstanceIdentifier"]
+    
     response = client.describe_db_instances(
-        DBInstanceIdentifier='ttsora20c',
+        DBInstanceIdentifier=dbname,
         )
     db_status = response["DBInstances"][0]["DBInstanceStatus"]
-    return(db_status)
+    return {
+        'db_status': str(db_status)
+        #'db_status': str(event)
+        }
+        
 
 
