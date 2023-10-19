482c09e39b001653191ce7d1b3cde18cf24af832
EC2 Default User <tonytsheng@gmail.com>
2023-10-19 19:26:37 +0000

    pgactive

--- POSTGRESQL/PGACTIVE/3NODES/1.node3.cr8server_mapping.sql
+++ POSTGRESQL/PGACTIVE/3NODES/1.node3.cr8server_mapping.sql
@@ -3,25 +3,25 @@ CREATE SERVER pgactive_server_endpoint3
     OPTIONS (host 'pg1003.cyt4dgtj55oy.us-east-2.rds.amazonaws.com', dbname 'app');
 CREATE USER MAPPING FOR postgres
     SERVER pgactive_server_endpoint3
-    OPTIONS (user 'postgres', password 'Pass1234');
+    OPTIONS (user 'postgres', password 'Pass');
 
 CREATE SERVER pgactive_server_endpoint2
     FOREIGN DATA WRAPPER pgactive_fdw
     OPTIONS (host 'pg1002.cyt4dgtj55oy.us-east-2.rds.amazonaws.com', dbname 'app');
 CREATE USER MAPPING FOR postgres
     SERVER pgactive_server_endpoint2
-    OPTIONS (user 'postgres', password 'Pass1234');
+    OPTIONS (user 'postgres', password 'Pass');
 
 CREATE SERVER pgactive_server_endpoint1
     FOREIGN DATA WRAPPER pgactive_fdw
     OPTIONS (host 'pg1001.cyt4dgtj55oy.us-east-2.rds.amazonaws.com', dbname 'app');
 CREATE USER MAPPING FOR postgres
     SERVER pgactive_server_endpoint1
-    OPTIONS (user 'postgres', password 'Pass1234');
+    OPTIONS (user 'postgres', password 'Pass');
 
 SELECT pgactive.pgactive_join_group(node_name := 'endpoint3-app'
-  , node_dsn := 'host=pg1003.cyt4dgtj55oy.us-east-2.rds.amazonaws.com dbname=app port=5432 user=postgres password=Pass1234'
-  , join_using_dsn := 'host=pg1001.cyt4dgtj55oy.us-east-2.rds.amazonaws.com dbname=app port=5432 user=postgres password=Pass1234');
+  , node_dsn := 'host=pg1003.cyt4dgtj55oy.us-east-2.rds.amazonaws.com dbname=app port=5432 user=postgres password=Pass'
+  , join_using_dsn := 'host=pg1001.cyt4dgtj55oy.us-east-2.rds.amazonaws.com dbname=app port=5432 user=postgres password=Pass');
 
 SELECT pgactive.pgactive_wait_for_node_ready();
 
