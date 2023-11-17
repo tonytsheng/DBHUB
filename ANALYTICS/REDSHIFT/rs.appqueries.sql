f19fded579331e3677df6a0803282b86a6969605
EC2 Default User <tonytsheng@gmail.com>
2023-11-17 17:23:44 +0000

    rdsproxy

--- ANALYTICS/REDSHIFT/rs.appqueries.sql
+++ ANALYTICS/REDSHIFT/rs.appqueries.sql
@@ -9,3 +9,7 @@ group by 1;
 select datepart(day,pickup_datetime) ,count(*) from adb305.ttsheng_nypub
 group by 1 order by 1;
 
+SELECT * FROM table ORDER BY index LIMIT 100 OFFSET 300
+-- read 100 rows but skip first 300
+
+-- Table partitioning is not supported, instead create granular tables and union
