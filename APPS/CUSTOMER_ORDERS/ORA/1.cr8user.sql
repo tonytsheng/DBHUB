2b40dc842f4fb2e50f9a1905c97aba04cd439495
EC2 Default User <tonytsheng@gmail.com>
2024-01-24 15:38:56 +0000

    dms promote

--- APPS/CUSTOMER_ORDERS/ORA/1.cr8user.sql
+++ APPS/CUSTOMER_ORDERS/ORA/1.cr8user.sql
@@ -1,6 +1,6 @@
 drop user customer_orders;
 create tablespace customer_orders;
-create user customer_orders identified by default tablespace customer_orders;
+create user customer_orders identified by "Pass" default tablespace customer_orders;
 grant create table to customer_orders;
 grant create any sequence to customer_orders;
 grant create any view to customer_orders;
--- APPS/FLY1/NEP/ins.airroutes.py
+++ APPS/FLY1/NEP/ins.airroutes.py
@@ -15,40 +15,28 @@ from random import randint
 #print ("query: " + user_query )
 
 WDIR='/home/ec2-user/DBHUB/APPS/FLY1/'
-INFILE=WDIR+'airport-codes_csv.csv'
+INFILE=WDIR+'air-routes-latest-nodes.csv'
 
-cmd='/bin/grep large_airport ' + INFILE + ' | /usr/bin/shuf -n1 | /usr/bin/awk -F\',\' \'{print $10}\' '
+#cmd='/bin/grep large_airport ' + INFILE + ' | /usr/bin/shuf -n1 | /usr/bin/awk -F\',\' \'{print $10}\' '
+cmd='/usr/bin/shuf -n1 ' + INFILE + ' | /usr/bin/awk -F\',\' \'{print $4}\' '
 DEP = subprocess.run(cmd, shell=True, capture_output=True, text=True ).stdout.strip("\n")
 ARR = subprocess.run(cmd, shell=True, capture_output=True, text=True ).stdout.strip("\n")
-print (DEP)
-print (ARR)
+#print (DEP)
+#print (ARR)
+distance = randint(0,99999)
 
 graph = Graph()
 remoteConn = DriverRemoteConnection('wss://nep100.cluster-cyt4dgtj55oy.us-east-2.neptune.amazonaws.com:8182/gremlin','g')
 g = graph.traversal().withRemote(remoteConn)
 
-#result = g.V().has('code', 'BWI').valueMap(True).next()
-#result = g.V().has('code','BWI').out().path().by('code')
-
-#result2 = g.addE('route').from('BWI').to('ATL')
-#result2 = g.addE('route').from(_.V('BWI')).to(_.V('ATL'))
-#result2 = g.addV('airport').property('name','TEST').property('code','YYY')
-#result2 = g.V().has('code', 'BWI').addE('route').to(V().has('code', 'YYY')).iterate()
-#print (result2)
-
-from_id = '6'
-to_id = '1450'
-distance = randint(0,99999)
-#g.V(from_id).addE('route').to(__.V(to_id)).property('distance', 99999).next() -- works
-#g.V().has('code','BWI').addE('route').to(__.V(to_id)).property('distance', 99999).next() -- works
-
 # want to do two inserts but one of them is always BWI as departure
 # building the graph
-
 g.V().has('code','BWI').addE('route').to(__.V().has('code',ARR)).property('distance', distance).next() 
 g.V().has('code',DEP).addE('route').to(__.V().has('code',ARR)).property('distance', distance).next() 
+g.V().has('code',DEP).addE('route').to(__.V().has('code',ARR)).property('distance', distance).next() 
+g.V().has('code',DEP).addE('route').to(__.V().has('code',ARR)).property('distance', distance).next() 
+g.V().has('code',DEP).addE('route').to(__.V().has('code',ARR)).property('distance', distance).next() 
 
-#result = g.V().has('code','BWI').out().has('code','YYY').valueMap()
 i=0
 result = g.V().has('code','BWI').out().path().by('code')
 for r in result:
@@ -61,5 +49,17 @@ remoteConn.close()
 
 
 #  g.addE('knows').from(vMarko).to(vPeter) ////
+#result = g.V().has('code', 'BWI').valueMap(True).next()
+#result = g.V().has('code','BWI').out().path().by('code')
+#result2 = g.addE('route').from('BWI').to('ATL')
+#result2 = g.addE('route').from(_.V('BWI')).to(_.V('ATL'))
+#result2 = g.addV('airport').property('name','TEST').property('code','YYY')
+#result2 = g.V().has('code', 'BWI').addE('route').to(V().has('code', 'YYY')).iterate()
+#result = g.V().has('code','BWI').out().has('code','YYY').valueMap()
+#print (result2)
+from_id = '6'
+to_id = '1450'
+#g.V(from_id).addE('route').to(__.V(to_id)).property('distance', 99999).next() -- works
+#g.V().has('code','BWI').addE('route').to(__.V(to_id)).property('distance', 99999).next() -- works
 
 
