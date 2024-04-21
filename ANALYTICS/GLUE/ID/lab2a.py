806acbdb5c641335b0f093b76930d0761f3d2203
EC2 Default User <tonytsheng@gmail.com>
2024-04-21 21:59:56 +0000

    glue_id

---
+++ ANALYTICS/GLUE/ID/lab2a.py
@@ -0,0 +1,12 @@
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+  
+sc = SparkContext.getOrCreate()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+
