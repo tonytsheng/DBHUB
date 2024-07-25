55af5d87d1bcb07f02193038037ab8bb89c10ade
EC2 Default User <tonytsheng@gmail.com>
2024-07-25 01:03:03 +0000

    ddb kinesis redshift

---
+++ SOLUTIONS_ARCH/DDB_KINESIS_REDSHIFT/3.samplequeries.sql
@@ -0,0 +1,31 @@
+SET enable_case_sensitive_identifier to TRUE;
+refresh materialized view demo_stream_vw;
+
+SELECT
+  EXTRACT( HOUR FROM approximate_arrival_timestamp) AS hour
+  , payload."dynamodb"."NewImage"."arr"."S"::varchar as arrival
+  , COUNT(*) AS count
+FROM
+  demo_stream_vw
+GROUP BY
+  hour
+  , arrival
+ORDER BY
+  hour
+  , arrival;
+
+
+SELECT
+  EXTRACT( DAY FROM approximate_arrival_timestamp) AS day
+  , payload."dynamodb"."NewImage"."arr"."S"::varchar as arrival
+  , COUNT(*) AS count
+FROM
+  demo_stream_vw
+GROUP BY
+  day
+  , arrival
+ORDER BY
+  day
+  , arrival;
+
+
